09b1e28219fd286f0fdce8dc1c3e591845e8f60c
Alex Kobayashi <alexkobayashi10@gmail.com>
2020-03-06 17:11:48 -0300

    Adicionado try para eventuais erros

--- download_from_url_BATCH.py
+++ download_from_url_BATCH.py
@@ -4,7 +4,7 @@ import pathlib
 import os
 
 # Input Data
-station_code_list = [46400000, 50230000]
+station_code_list = [46400000,1373001, 50230000]
 
 # Creation of Folder for the data
 path_folder = pathlib.Path(r"C:\Users\Usuario\Desktop\lhc_hidroweb")
@@ -24,19 +24,22 @@ for code in station_code_list:
     print(r.status_code)
 
     # Unzipping the first folder and extracting on the folder
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall(path_folder)
+    try:
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path_folder)
 
-    # Unzipping the rest of the Zips and deleting the Zip files
-    for path, dir_list, file_list in os.walk(path_folder):
-        for file_name in file_list:
-            if file_name.endswith(".zip"):
-                abs_file_path = os.path.join(path, file_name)
-                parent_path = os.path.split(abs_file_path)[0]
-                output_folder_name = os.path.splitext(abs_file_path)[0]
-                output_path = os.path.join(parent_path, output_folder_name)
+        # Unzipping the rest of the Zips and deleting the Zip files
+        for path, dir_list, file_list in os.walk(path_folder):
+            for file_name in file_list:
+                if file_name.endswith(".zip"):
+                    abs_file_path = os.path.join(path, file_name)
+                    parent_path = os.path.split(abs_file_path)[0]
+                    output_folder_name = os.path.splitext(abs_file_path)[0]
+                    output_path = os.path.join(parent_path, output_folder_name)
 
-                zip_obj = zipfile.ZipFile(abs_file_path, 'r')
-                zip_obj.extractall(output_path)
-                zip_obj.close()
-                os.remove(abs_file_path)
+                    zip_obj = zipfile.ZipFile(abs_file_path, 'r')
+                    zip_obj.extractall(output_path)
+                    zip_obj.close()
+                    os.remove(abs_file_path)
+    except:
+        print("Error on the CODE: {}".format(code))
