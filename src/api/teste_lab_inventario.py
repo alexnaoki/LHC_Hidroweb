729f0edb9da514966f5be9a6668736d953cbe329
alexnaoki <alexkobayashi10@gmail.com>
2020-04-23 21:49:08 -0300

    .

--- src/api/teste_lab_inventario.py
+++ src/api/teste_lab_inventario.py
@@ -2,87 +2,62 @@ import xml.etree.ElementTree as ET
 import requests
 import pandas as pd
 
-api_inventario = 'http://telemetriaws1.ana.gov.br/ServiceANA.asmx/HidroInventario'
+def teste():
 
-params = {'codEstDE':'',
-          'codEstATE':'',
-          'tpEst':'',
-          'nmEst':'',
-          'nmRio':'',
-          'codSubBacia':'',
-          'codBacia':'',
-          'nmMunicipio':'',
-          'nmEstado':'',
-          'sgResp':'',
-          'sgOper':'',
-          'telemetrica':''}
+    api_inventario = 'http://telemetriaws1.ana.gov.br/ServiceANA.asmx/HidroInventario'
 
-response = requests.get(api_inventario, params)
+    params = {'codEstDE':'',
+              'codEstATE':'',
+              'tpEst':'',
+              'nmEst':'',
+              'nmRio':'',
+              'codSubBacia':'',
+              'codBacia':'',
+              'nmMunicipio':'',
+              'nmEstado':'',
+              'sgResp':'',
+              'sgOper':'',
+              'telemetrica':''}
 
-print(response)
+    response = requests.get(api_inventario, params)
 
-tree = ET.ElementTree(ET.fromstring(response.content))
+    print(response)
 
-root = tree.getroot()
+    tree = ET.ElementTree(ET.fromstring(response.content))
 
-data = {'BaciaCodigo':[],'SubBaciaCodigo':[],'RioCodigo':[],'RioNome':[],'EstadoCodigo':[],
-        'nmEstado':[],'MunicipioCodigo':[],'nmMunicipio':[],'ResponsavelCodigo':[],
-        'ResponsavelSigla':[],'ResponsavelUnidade':[],'ResponsavelJurisdicao':[],
-        'OperadoraCodigo':[],'OperadoraSigla':[],'OperadoraUnidade':[],'OperadoraSubUnidade':[],
-        'TipoEstacao':[],'Codigo':[],'Nome':[],'CodigoAdicional':[],'Latitude':[],'Longitude':[],
-        'Altitude':[],'AreaDrenagem':[],'TipoEstacaoEscala':[],'TipoEstacaoRegistradorNivel':[],
-        'TipoEstacaoDescLiquida':[],'TipoEstacaoSedimentos':[],
-        'TipoEstacaoQualAgua':[],
-        'TipoEstacaoPluviometro':[],
-        'TipoEstacaoRegistradorChuva':[],
-        'TipoEstacaoTanqueEvapo':[],
-        'TipoEstacaoClimatologica':[],
-        'TipoEstacaoPiezometria':[],
-        'TipoEstacaoTelemetrica':[],
-        'PeriodoEscalaInicio':[],
-        'PeriodoEscalaFim':[] ,
-        'PeriodoRegistradorNivelInicio' :[],
-        'PeriodoRegistradorNivelFim' :[],
-        'PeriodoDescLiquidaInicio' :[],
-        'PeriodoDescLiquidaFim':[] ,
-        'PeriodoSedimentosInicio' :[],
-        'PeriodoSedimentosFim':[] ,
-        'PeriodoQualAguaInicio':[] ,
-        'PeriodoQualAguaFim' :[],
-        'PeriodoPluviometroInicio':[] ,
-        'PeriodoPluviometroFim':[] ,
-        'PeriodoRegistradorChuvaInicio' :[],
-        'PeriodoRegistradorChuvaFim' :[],
-        'PeriodoTanqueEvapoInicio':[] ,
-        'PeriodoTanqueEvapoFim':[] ,
-        'PeriodoClimatologicaInicio' :[],
-        'PeriodoClimatologicaFim':[] ,
-        'PeriodoPiezometriaInicio':[] ,
-        'PeriodoPiezometriaFim' :[],
-        'PeriodoTelemetricaInicio' :[],
-        'PeriodoTelemetricaFim' :[],
-        'TipoRedeBasica' :[],
-        'TipoRedeEnergetica' :[],
-        'TipoRedeNavegacao' :[],
-        'TipoRedeCursoDagua' :[],
-        'TipoRedeEstrategica':[] ,
-        'TipoRedeCaptacao':[] ,
-        'TipoRedeSedimentos':[] ,
-        'TipoRedeQualAgua':[] ,
-        'TipoRedeClasseVazao':[] ,
-        'UltimaAtualizacao':[] ,
-        'Operando':[] ,
-        'Descricao':[] ,
-        'NumImagens':[] ,
-        'DataIns':[] ,
-        'DataAlt':[]}
+    root = tree.getroot()
 
-# print(root.tag)
-for i in root.iter('Table'):
-    print(i.find('ResponsavelSigla').text)
-    print(i.find('Codigo').text)
-    code = i.find('Codigo').text
-    print('{:08}'.format(int(code)))
-    # print(i.find('Latitude').text)
-    # print(i.find('Longitude').text)
-    # print(i.find('AreaDrenagem').text)
+    data = {'BaciaCodigo':[],'SubBaciaCodigo':[],'RioCodigo':[],'RioNome':[],'EstadoCodigo':[],
+            'nmEstado':[],'MunicipioCodigo':[],'nmMunicipio':[],'ResponsavelCodigo':[],
+            'ResponsavelSigla':[],'ResponsavelUnidade':[],'ResponsavelJurisdicao':[],
+            'OperadoraCodigo':[],'OperadoraSigla':[],'OperadoraUnidade':[],'OperadoraSubUnidade':[],
+            'TipoEstacao':[],'Codigo':[],'Nome':[],'CodigoAdicional':[],'Latitude':[],'Longitude':[],
+            'Altitude':[],'AreaDrenagem':[],'TipoEstacaoEscala':[],'TipoEstacaoRegistradorNivel':[],
+            'TipoEstacaoDescLiquida':[],'TipoEstacaoSedimentos':[],'TipoEstacaoQualAgua':[],
+            'TipoEstacaoPluviometro':[],'TipoEstacaoRegistradorChuva':[],'TipoEstacaoTanqueEvapo':[],
+            'TipoEstacaoClimatologica':[],'TipoEstacaoPiezometria':[],'TipoEstacaoTelemetrica':[],'PeriodoEscalaInicio':[],'PeriodoEscalaFim':[] ,
+            'PeriodoRegistradorNivelInicio' :[],'PeriodoRegistradorNivelFim' :[],'PeriodoDescLiquidaInicio' :[],'PeriodoDescLiquidaFim':[] ,'PeriodoSedimentosInicio' :[],
+            'PeriodoSedimentosFim':[] ,'PeriodoQualAguaInicio':[] ,'PeriodoQualAguaFim' :[],'PeriodoPluviometroInicio':[] ,'PeriodoPluviometroFim':[] ,
+            'PeriodoRegistradorChuvaInicio' :[],'PeriodoRegistradorChuvaFim' :[],'PeriodoTanqueEvapoInicio':[] ,'PeriodoTanqueEvapoFim':[] ,'PeriodoClimatologicaInicio' :[],'PeriodoClimatologicaFim':[] ,
+            'PeriodoPiezometriaInicio':[] ,'PeriodoPiezometriaFim' :[],'PeriodoTelemetricaInicio' :[],'PeriodoTelemetricaFim' :[],
+            'TipoRedeBasica' :[],'TipoRedeEnergetica' :[],'TipoRedeNavegacao' :[],'TipoRedeCursoDagua' :[],
+            'TipoRedeEstrategica':[] ,'TipoRedeCaptacao':[] ,'TipoRedeSedimentos':[] ,'TipoRedeQualAgua':[] ,
+            'TipoRedeClasseVazao':[] ,'UltimaAtualizacao':[] ,'Operando':[] ,'Descricao':[] ,'NumImagens':[] ,'DataIns':[] ,'DataAlt':[]}
+
+    # print(root.tag)
+    for i in root.iter('Table'):
+
+        for j in data.keys():
+            d = i.find('{}'.format(j)).text
+            if j == 'Codigo':
+                data['{}'.format(j)].append('{:08}'.format(int(d)))
+            else:
+                data['{}'.format(j)].append(d)
+
+    print(len(list(root.iter('Table'))))
+    # print(data)
+    df = pd.DataFrame(data)
+    df.to_csv('data_invent.csv')
+
+
+teste()
